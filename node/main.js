2c2571e37fb37247b0a520476a4197022e19346c
张世博 <zhangshibo@xdf.cn>
2021-02-22 10:26:28 +0800

    测试

--- node/main.js
+++ node/main.js
@@ -10,14 +10,14 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.static(path.resolve(__dirname,'build')))
 
 
-app.all('*',function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
-    res.header("Content-Type", "application/json;charset=utf-8");
-    next();
-})
+// app.all('*',function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild");
+//     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+//     res.header("X-Powered-By",' 3.2.1')
+//     res.header("Content-Type", "application/json;charset=utf-8");
+//     next();
+// })
 
 
 app.get('/taglist',async function(req,res){
